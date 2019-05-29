83938546d34a7902ae81a9d2029567b28bdcc859
Liudas Stonys <liudas.stonys@gmail.com>
2019-05-29 21:10:38 +0300

    Updated email verification

--- config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("EMAIL", "liudas.stonys@gmail.com");
-define("PASSWORD", "nxt4v3g00gl3");
-
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "php-login-system");
